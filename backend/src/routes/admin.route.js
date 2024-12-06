379c66ea27611fe8a06ff6e749b3616917933ec5
Pham Viet Hoa <phamviethoa2416@gmail.com>
2024-12-06 07:57:14 +0700

    feat: add the function for admin

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
+import { createSong, deleteSong, createPlaylist, deletePlaylist, checkAdmin } from "../controllers/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", protectRoute, requireAdmin, createSong);
+router.use(protectRoute, requireAdmin);
+
+router.get("/check", checkAdmin);
+
+router.post("/songs", createSong);
+router.delete("/songs/:id", deleteSong);
+
+router.post("/playlists", createPlaylist);
+router.delete("/playlists/:id", deletePlaylist);
 
 export default router;
