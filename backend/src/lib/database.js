55d0961ba5dac82091da74823d344dc9d2980b43
Pham Viet Hoa <phamviethoa2416@gmail.com>
2024-12-06 07:55:28 +0700

    fix: fix the error in logging

--- backend/src/lib/database.js
+++ backend/src/lib/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         const connection = await mongoose.connect(process.env.MONGODB_URI); 
-        console.log("Connected to MongoDB ${connection.connection.host}");
+        console.log(`Connected to MongoDB ${connection.connection.host}`);
     } catch (error) {
         console.log("Failed to connect to MongoDB", error);
         process.exit(1);
