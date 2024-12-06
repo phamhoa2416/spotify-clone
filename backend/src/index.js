b23300078ab47f3430e7c6198c33fccc0147f824
Pham Viet Hoa <phamviethoa2416@gmail.com>
2024-12-06 07:59:31 +0700

    feat: add routes and middleware for the app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,43 @@
 import express from "express";
 import dotenv from "dotenv";
-
 import { clerkMiddleware } from '@clerk/express'
+import fileUpload from "express-fileupload";
+import path from "path";
+
 import { connectDB } from "./lib/database.js";
 import authRoutes from "./routes/auth.route.js";
 import adminRoutes from "./routes/admin.route.js";
+import playlistRoutes from "./routes/playlist.route.js";
+import songRoutes from "./routes/song.route.js";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "temp"),
+    createParentPath: true,
+    limits: {
+        fileSize: 10 * 1024 * 1024
+    }
+})
+);
+
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message});
+}) 
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/playlists", playlistRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
