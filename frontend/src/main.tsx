35a7c1cde152e20984c3ee7174f685e9ea4955df
Pham Viet Hoa <phamviethoa2416@gmail.com>
2024-12-16 09:10:38 +0700

    feat: add providers for main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ClerkProvider } from "@clerk/clerk-react";
 import './index.css'
 import App from './App.tsx'
+import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter } from 'react-router-dom';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -12,8 +13,10 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
-				<App />
-		</ClerkProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl= '/'> 
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
 )
