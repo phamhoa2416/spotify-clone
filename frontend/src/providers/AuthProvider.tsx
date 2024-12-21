0fd15b0a5fc5b785408592644fef19d59473a1e4
Pham Viet Hoa <phamviethoa2416@gmail.com>
2024-12-21 23:04:06 +0700

    feat: add auth provider for authentication

---
+++ frontend/src/providers/AuthProvider.tsx
@@ -0,0 +1,40 @@
+import { axiosInstance } from "@/lib/axios"
+import { useAuth } from "@clerk/clerk-react"
+import { LoaderCircle } from "lucide-react"
+import { useEffect, useState } from "react"
+
+const updateAPIToken = (token: string | null) => {
+    if (token) axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`
+    else delete axiosInstance.defaults.headers.common["Authorization"]
+}
+
+const AuthProvider = ({children}: {children: React.ReactNode}) => {
+    const { getToken } = useAuth()
+    const [ loading, setLoading ] = useState(true)
+
+    useEffect(() => {
+        const initAuth = async () => {
+            try {
+                const token = await getToken()
+                updateAPIToken(token)
+            } catch (error: any) {
+                updateAPIToken(null)
+                console.log("Error in auth provider", error)
+            } finally {
+                setLoading(false)
+            }
+        };
+
+        initAuth()
+    }, [getToken]);
+
+    if (loading) return (
+        <div className="h-screen w-full flex items-center justify-center">
+            <LoaderCircle className="size-8 text-purple-600 animate-spin"/>
+        </div>
+    );
+
+    return <> {children} </>
+}
+
+export default AuthProvider
