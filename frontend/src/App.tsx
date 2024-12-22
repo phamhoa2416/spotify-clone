d2baff69cbb2ec42c0abdc545195f9c568237552
Pham Viet Hoa <phamviethoa2416@gmail.com>
2024-12-22 08:26:16 +0700

    feat: add the main layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,21 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage"
 import AuthCallbackPage from "./pages/auth/AuthCallbackPage"
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
+import MainLayout from "./layout/MainLayout"
 
 function App() {
   return (
     <>
       <Routes>
-          <Route path="/" element={ <HomePage /> } />
           <Route 
           path="/sso-callback" 
           element= {<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
           /> 
           <Route path="/auth-callback" element= { <AuthCallbackPage /> } />
+
+          <Route element= {<MainLayout/>}>
+            <Route path="/" element={ <HomePage /> } />
+          </Route>
       </Routes>
     </>
   )
