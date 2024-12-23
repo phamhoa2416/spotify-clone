a57644299d1d72c11fbe333b1af2408d2122af49
Pham Viet Hoa <phamviethoa2416@gmail.com>
2024-12-23 08:33:08 +0700

    feat: create main layout

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,24 +1,29 @@
-import { ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
 import { Outlet } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 
 const MainLayout = () => {
     const isMobile = false;
   return (
     <div className='h-screen bg-black text-white flex flex-col'>
-      <ResizablePanelGroup>
+      <ResizablePanelGroup direction="horizontal" className="flex-1 flex h-full overflow-hidden p-2">
         {/* Left Sidebar component */}
         <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
-
+            <Sidebar />
         </ResizablePanel>
+
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
 
         {/* Main content */}
         <ResizablePanel defaultSize={isMobile ? 80 : 60}>
             <Outlet />
         </ResizablePanel>
 
-        {/* Right Sidebar component */}
-        <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
 
+        {/* Right Sidebar component */}
+        <ResizablePanel defaultSize={20} minSize={0} maxSize={30} collapsedSize={0}>
+            Right sidebar
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
