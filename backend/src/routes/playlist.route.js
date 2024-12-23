a4b528b65739270cabf3154fdf5476acc9b17c3a
Pham Viet Hoa <phamviethoa2416@gmail.com>
2024-12-23 08:31:48 +0700

    refactor: add routes for objects

--- backend/src/routes/playlist.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAllPlaylists, getPlaylistById } from "../controllers/playlist.controller.js";
-
-const router = Router();
-
-router.get("/", getAllPlaylists);
-router.get("/:playlistId", getPlaylistById);
-
-export default router;
